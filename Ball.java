f30112590c99259a536471cfa646b53995c6d413
Teddybandit <teddytacobandit@gmail.com>
2023-01-27 09:50:25 -0600

    intellij commit

--- Ball.java
+++ Ball.java
@@ -2,11 +2,14 @@ import java.lang.Math;
 import java.awt.*;
 public class Ball{
     private double x,y,xSpeed,ySpeed;
-    private static int nextID;
+    private static int nextID;//gives each ball a unique ID
     private Color color;
     private int ID;
-    private int lastBounce = -1;
-    private int size; 
+    private int lastBounce = -1;//The ID of the last ball it hit
+    //set to -1 when it hits a wall
+    //cant bounce off the ball until it bounces of something else
+    private int size;
+    //constructors
     public Ball(double x,double y,double xSpeed,double ySpeed){
         this.x = x;
         this.y = y;
@@ -15,7 +18,7 @@ public class Ball{
         ID=nextID;
         nextID++;
         color = randomColor();
-        size=(int)(Math.random()*11+10);
+        size=(int)(Math.random()*11+10);//random size from 10-20
     }
     public Ball(){
         x=(Math.random()*480+10);
@@ -36,18 +39,19 @@ public class Ball{
     public void move(){
         x+=xSpeed;
         y+=ySpeed;
-        ySpeed+=.00;
+        ySpeed+=.01;//if you want to add gravity
     }
     static void collide(Ball b1,Ball b2){//has 2 balls bounce off eachother if they are touching
         double xdis = b1.x-b2.x;
         double ydis = b1.y-b2.y;
         int sizeTotal = b1.size+b2.size;
+        //first two checks are redundant but conserve processing power
         if(Math.abs(xdis)<sizeTotal
                 &&Math.abs(ydis)<sizeTotal
                 &&((b2.lastBounce!=b1.ID)||(b1.lastBounce!=b2.ID))
                 &&Math.pow(xdis,2)+Math.pow(ydis,2)<Math.pow(sizeTotal,2)
                 ){
-            double xvel = b1.xSpeed-b2.xSpeed;
+            double xvel = b1.xSpeed-b2.xSpeed;//if you want an explanation take physics
             double yvel = b1.ySpeed-b2.ySpeed;
             double velocity = Math.sqrt(Math.pow(xvel,2)+Math.pow(yvel,2));
             double touch = Math.atan2(ydis , xdis);
@@ -57,7 +61,7 @@ public class Ball{
             b2.xSpeed-=bounce*velocity*xdis/distence;
             b1.ySpeed+=bounce*velocity*ydis/distence;
             b2.ySpeed-=bounce*velocity*ydis/distence;
-            b1.lastBounce=b2.ID;
+            b1.lastBounce=b2.ID;//ensures they dont bounce off eachother again
             b2.lastBounce=b1.ID;
             //debug messages
             /*System.out.println("xvel "+xvel+"\nyvel "+yvel+"\ntouch"+touch+"\nbounce"+bounce);
@@ -68,6 +72,9 @@ public class Ball{
         }
 
     }
+    /*using absolute value instead of *=-1 ensures they dont get trapped outside the boundaries.
+    lastBounce =-1 lets them bounce off anything
+     */
     public void wallBounce(){
         if (x<10){
             xSpeed=Math.abs(xSpeed);
@@ -84,6 +91,7 @@ public class Ball{
             lastBounce=-1;
         }
     }
+    //generates a random color with one value above 192 to make it more vibrant
     private Color randomColor(){
         int high = (int)(Math.random()*3);
         int[] RGB = new int[3];
